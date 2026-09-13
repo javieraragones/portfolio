2831d3a66d2391138ded6613e568a81fad5f0a29
Javier <116835793+javieraragones@users.noreply.github.com>
2026-09-13 19:46:02 +0200

    Change ScrollUp from anchor to button element
    Refactor ScrollUp component to use a button instead of an anchor tag for scrolling.

--- src/components/scrollup/ScrollUp.jsx
+++ src/components/scrollup/ScrollUp.jsx
@@ -4,14 +4,19 @@ import "./scrollup.css";
 const ScrollUp = () => {
   window.addEventListener("scroll", function () {
     const scrollUp = this.document.querySelector(".scrollup");
-    // When the scroll is higher tham 560 viewport height, add the show-scroll class to a tag with the scroll-top class
+
     if (this.scrollY >= 560) scrollUp.classList.add("show-scroll");
     else scrollUp.classList.remove("show-scroll");
   });
+
   return (
-    <a href="#" className="scrollup">
+    <button
+      type="button"
+      className="scrollup"
+      onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
+    >
       <i className="uil uil-arrow-up scrollup__icon"></i>
-    </a>
+    </button>
   );
 };
 
